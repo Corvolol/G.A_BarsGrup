2a7c48911b5d83f86bbef85b76c29924df7b016a
Adel <4811102076@mail.ru>
2022-03-18 16:12:39 +0300

    Add Task1

---
+++ BarsGrup/Task1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Class1
+    {
+        public event EventHandler<char>? OnKeyPressed;
+        public void Run()
+        {
+            bool exitPressed = true;
+            while (exitPressed)
+            {
+                var button = Console.ReadKey();
+                OnKeyPressed?.Invoke(this, button.KeyChar);
+                switch (char.ToLower(button.KeyChar))
+                {
+                    case 'c':
+                        exitPressed = false;
+                        break;
+                    case 'с':
+                        exitPressed = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ BarsGrup/Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿using Task1;
+
+Class1 test = new();
+test.OnKeyPressed += (sender, buttonKey) => Console.WriteLine($"\nвы нажали {buttonKey}");
+test.Run();
